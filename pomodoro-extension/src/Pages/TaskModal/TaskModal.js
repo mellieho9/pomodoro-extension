a5da7384740e9e039b4a2fe0690cb873c853f110
rina.easterday@gmail.com <rina.easterday@gmail.com>
2022-12-17 04:02:43 -0800

    Created TaskModal with webcam integration (see notes about webcam integration)

--- pomodoro-extension/src/Pages/TaskModal/TaskModal.js
+++ pomodoro-extension/src/Pages/TaskModal/TaskModal.js
@@ -1,5 +1,7 @@
-import React, { useState, useEffect, useRef } from 'react';
+/*global chrome*/
+import React, { useEffect, useRef } from 'react';
 import Tasks from "./Tasks.json";
+import { Backdrop } from '@mui/material';
 
 function TaskModal() {
     const randomTaskId = Math.floor(Math.random() * (Tasks.length));
@@ -7,17 +9,26 @@ function TaskModal() {
     const videoRef = useRef(null);
 
     useEffect(() => {
-        
-        navigator.mediaDevices.getUserMedia({ video: {width: 200, height: 200} })
-        .then(stream => {
-            videoRef.current.srcObject = stream;
+        // NOTE: can't get user permission for webcam with chrome:// 
+        // chrome only allows access from secure sources such as HTTP or localhost
+        navigator.mediaDevices.getUserMedia({ audio: false, video: {width: 200, height: 200} })
+        .then((stream) => {
+            let video = videoRef.current;
+            video.srcObject = stream;
+            video.play();
+        }).catch ((err) => {
+            alert(`Error with loading webcam (${err})`);
         });
     }, []);
     return (
-      <div>
-        <p>{taskJson.task_description}</p>
-        <video style={{borderRadius: 120, borderStyle: 'solid', borderColor: 'purple'}} ref={videoRef} autoPlay />
-      </div>
+        <Backdrop 
+            open
+            sx={{ color: '#fff', background: 'rgba(59,39,99,0.9)', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            style={{display: 'block', padding: '4em'}}
+        >
+            <p>Task: {taskJson.task_description}</p>
+            <video style={{borderRadius: 120, borderStyle: 'solid', borderColor: 'purple'}} ref={videoRef} autoPlay />
+        </Backdrop>
     );
   }
 export default TaskModal;
